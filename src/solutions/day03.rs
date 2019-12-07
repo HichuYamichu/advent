36b428af5eda2e21a27bc5bda27cc05d689420bf
HichuYamichu <hichuyamichu@gmail.com>
2019-12-07 19:35:15 +0100

    removed tests

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -71,17 +71,3 @@ fn find_points(wire_path: Vec<(char, i32)>) -> HashSet<Point> {
   }
   wire_points
 }
-
-#[test]
-pub fn tests_a() {
-  assert_eq!(
-    a("R75,D30,R83,U83,L12,D49,R71,U7,L72
-  U62,R66,U55,R34,D71,R55,D58,R83"),
-    ""
-  );
-}
-
-#[test]
-fn tests_b() {
-  assert_eq!(b(""), "");
-}
--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -5,13 +5,3 @@ pub fn a(_input: &str) -> String {
 pub fn b(_input: &str) -> String {
   String::from("")
 }
-
-#[test]
-pub fn tests_a() {
-  assert_eq!(a(""), "");
-}
-
-#[test]
-fn tests_b() {
-  assert_eq!(b(""), "");
-}
